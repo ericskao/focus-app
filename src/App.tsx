4b820fed4be5b77e2790b2b57446134eedd79f87
Eric K <ericskao@gmail.com>
2022-10-08 17:40:32 -0700

    add sass, fontawesome, howler

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import './App.css';
+import './App.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+import useSound from 'use-sound';
+import boopSfx from '../src/assets/audio/sample.mp3';
+import boopSfx2 from '../src/assets/audio/sample-12s.mp3';
+import Header from './components/Header';
 
 function App() {
-  const onButtonClick = () => {
-    console.log('clicked');
-  };
+  // const [play] = useSound(boopSfx);
+  // const [play2] = useSound(boopSfx2);
 
   return (
     <div className="App">
-      <button onClick={onButtonClick}>click to play</button>
+      <Header />
+      <div>stufff</div>
+      <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import './Header.scss';
+
+const Header = () => {
+  return (
+    <div className="Header text-white flex justify-end gap-2">
+      <div>About</div>
+      <div>Contact</div>
+      <div>Login</div>
+    </div>
+  );
+};
+
+export default Header;
